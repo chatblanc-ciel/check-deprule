2b32573e3eb539260691e7ff1ae1f2e14447ac7e
chatblanc-ciel <matsumoto9845@spice.ocn.ne.jp>
2025-03-13 07:18:25 +0900

    tune excutable function

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,42 @@
+use std::{env, path::Path};
+
 pub mod dependency_graph;
 pub mod dependency_rule;
 pub mod metadata;
+
+pub fn handler(
+    graph_build_configs: dependency_graph::DependencyGraphBuildConfigs,
+    metadata_configs: metadata::CollectMetadataConfig,
+) -> anyhow::Result<()> {
+    let metadata = metadata::collect_metadata(metadata_configs.clone())?;
+    let graph = dependency_graph::build_dependency_graph(metadata, graph_build_configs)?;
+
+    if let Some(manifest_path) = metadata_configs.manifest_path {
+        dependency_graph::tree::print(&graph, Path::new(&manifest_path))?;
+    } else {
+        let manifest_path = format!("{}/Cargo.toml", env::current_dir()?.display());
+        dependency_graph::tree::print(&graph, manifest_path)?;
+    }
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{dependency_graph::DependencyGraphBuildConfigs, metadata::CollectMetadataConfig};
+    use anyhow::Result;
+
+    #[test]
+    fn test_main() -> Result<()> {
+        let build_config = DependencyGraphBuildConfigs::default();
+        let collect_metadata_config = CollectMetadataConfig {
+            manifest_path: Some("tests/demo_crates/clean-arch/Cargo.toml".to_string()),
+            ..CollectMetadataConfig::default()
+        };
+
+        handler(build_config, collect_metadata_config)?;
+
+        Ok(())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,16 @@
 use anyhow::{Ok, Result};
 use check_deprule::{
-    dependency_graph::{DependencyGraphBuildConfigs, build_dependency_graph},
-    metadata::{CollectMetadataConfig, collect_metadata},
+    dependency_graph::DependencyGraphBuildConfigs, handler, metadata::CollectMetadataConfig,
 };
 
 fn main() -> Result<()> {
     let build_config = DependencyGraphBuildConfigs::default();
     let collect_metadata_config = CollectMetadataConfig {
-        manifest_path: Some("tests/demo_crates/clean-arch/Cargo.toml".to_string()),
+        manifest_path: None,
         ..CollectMetadataConfig::default()
     };
 
-    let metadata = collect_metadata(collect_metadata_config)?;
-    let graph = build_dependency_graph(metadata, build_config)?;
-
-    check_deprule::dependency_graph::tree::print(&graph)?;
+    handler(build_config, collect_metadata_config)?;
 
     Ok(())
 }
